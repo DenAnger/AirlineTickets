f175301d7a2aff0bd0c4e501be9ce198e67829be
slemeshaev <lemeshaevs@gmail.com>
2019-05-04 10:35:05 +0300

    Deleted mainviewcontroller

--- AirlineTickets/AirlineTickets/MainViewController.h
+++ AirlineTickets/AirlineTickets/MainViewController.h
@@ -1,15 +1,17 @@
 //
-//  ViewController.h
+//  MainViewController.h
 //  AirlineTickets
 //
-//  Created by Станислав Лемешаев on 03/05/2019.
+//  Created by Станислав Лемешаев on 04/05/2019.
 //  Copyright © 2019 Станислав Лемешаев. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface MainViewController : UIViewController
+NS_ASSUME_NONNULL_BEGIN
 
+@interface MainViewController : UIViewController
 
 @end
 
+NS_ASSUME_NONNULL_END
--- AirlineTickets/AirlineTickets/MainViewController.m
+++ AirlineTickets/AirlineTickets/MainViewController.m
@@ -1,8 +1,8 @@
 //
-//  ViewController.m
+//  MainViewController.m
 //  AirlineTickets
 //
-//  Created by Станислав Лемешаев on 03/05/2019.
+//  Created by Станислав Лемешаев on 04/05/2019.
 //  Copyright © 2019 Станислав Лемешаев. All rights reserved.
 //
 
@@ -19,5 +19,14 @@
     // Do any additional setup after loading the view.
 }
 
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 @end
