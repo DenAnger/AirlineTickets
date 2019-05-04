a4a067c9790203b6358ac4fb66402175ece804df
slemeshaev <lemeshaevs@gmail.com>
2019-05-04 10:38:56 +0300

    Created file country

---
+++ AirlineTickets/AirlineTickets/Objects/Country.h
@@ -0,0 +1,17 @@
+//
+//  Country.h
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 04/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Country : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AirlineTickets/AirlineTickets/Objects/Country.m
@@ -0,0 +1,13 @@
+//
+//  Country.m
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 04/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import "Country.h"
+
+@implementation Country
+
+@end
