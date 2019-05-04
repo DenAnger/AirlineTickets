ace85a04ea050e8873e4f4f3f1c0ba820de1a90b
slemeshaev <lemeshaevs@gmail.com>
2019-05-04 10:52:55 +0300

    Created file DataManager

---
+++ AirlineTickets/AirlineTickets/DataManager.h
@@ -0,0 +1,17 @@
+//
+//  DataManager.h
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 04/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DataManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
