cde8acd480d75eebe931e6abf3a87417a1f21f9f
slemeshaev <lemeshaevs@gmail.com>
2019-05-07 23:37:43 +0300

    Created files PlaceViewController and struct SearchRequest

---
+++ AirlineTickets/AirlineTickets/PlaceViewController.h
@@ -0,0 +1,17 @@
+//
+//  PlaceViewController.h
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 07/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PlaceViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AirlineTickets/AirlineTickets/SearchRequest.h
@@ -0,0 +1,13 @@
+//
+//  SearchRequest.h
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 07/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#ifndef SearchRequest_h
+#define SearchRequest_h
+
+
+#endif /* SearchRequest_h */
