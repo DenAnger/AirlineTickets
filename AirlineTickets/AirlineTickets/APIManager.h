0c323623643dbd3b6e24298fe9c0459bf8ac8d58
slemeshaev <lemeshaevs@gmail.com>
2019-05-13 23:53:00 +0300

    Add search for current location by ip-address

--- AirlineTickets/AirlineTickets/APIManager.h
+++ AirlineTickets/AirlineTickets/APIManager.h
@@ -7,11 +7,13 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "DataManager.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface APIManager : NSObject
-
++ (instancetype)sharedInstance;
+- (void)cityForCurrentIP:(void (^)(City *city))completion;
 @end
 
 NS_ASSUME_NONNULL_END
