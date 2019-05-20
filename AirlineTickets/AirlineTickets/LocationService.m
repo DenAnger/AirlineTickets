d73ee965fb0fa984de092dd2f9d2a1a47e2a7d3e
slemeshaev <lemeshaevs@gmail.com>
2019-05-20 13:56:20 +0300

    Created files location service

---
+++ AirlineTickets/AirlineTickets/LocationService.m
@@ -0,0 +1,13 @@
+//
+//  LocationService.m
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 20/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import "LocationService.h"
+
+@implementation LocationService
+
+@end
