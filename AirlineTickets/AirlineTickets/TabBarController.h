5999bd12a54d2394bcf3f941270ad3a7854abc21
slemeshaev <lemeshaevs@gmail.com>
2019-05-27 14:47:16 +0300

    Created files TabBarControllers

---
+++ AirlineTickets/AirlineTickets/TabBarController.h
@@ -0,0 +1,17 @@
+//
+//  TabBarController.h
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 27/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TabBarController : UITabBarController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ AirlineTickets/AirlineTickets/TabBarController.m
@@ -0,0 +1,32 @@
+//
+//  TabBarController.m
+//  AirlineTickets
+//
+//  Created by Станислав Лемешаев on 27/05/2019.
+//  Copyright © 2019 Станислав Лемешаев. All rights reserved.
+//
+
+#import "TabBarController.h"
+
+@interface TabBarController ()
+
+@end
+
+@implementation TabBarController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
